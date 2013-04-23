a419085f105de08a3f0b763505584823ce9ab357
Perry Roper <perryroper@gmail.com>
2013-04-23 22:31:31 +0100

    Poll a URL from the results page to see if the next question is active

--- simplelivepoll/static/scripts/main.js
+++ simplelivepoll/static/scripts/main.js
@@ -1,5 +1,31 @@
-$(function() {
+var results = {
+    init: function(args) {
+        var self = this;
 
+        var url = args.next;
+        if(url) {
+            self.pollNext(url);
+        }
+    },
+    pollNext: function(url) {
+        // If the next question is not yet active a 404 is returned. Easy-peasy.
 
+        // Polling interval in ms
+        var interval = 1000;
 
-});
+        var request = function() {
+            $.ajax({
+                type: 'get',
+                url: url
+            }).always(function(data) {
+                if(data.status == '404') {
+                    window.setTimeout(request, interval);
+                } else {
+                    alert('The next question is active!');
+                }
+            });
+        };
+        request();
+    }
+};
+
