aa1c9b31b368ecbb5d468f609a341551b43dc1a8
Perry Roper <perryroper@gmail.com>
2013-04-24 22:39:08 +0100

    Render highcharts, poll the results url and update

--- simplelivepoll/static/scripts/main.js
+++ simplelivepoll/static/scripts/main.js
@@ -26,6 +26,60 @@ var results = {
             });
         };
         request();
+    },
+    loadResults: function() {
+        var results;
+        $.ajax({
+            url: window.location,
+            type: 'get',
+            async: false,
+            cache: false
+        }).success(function(data) {
+            results = data;
+        });
+        return results;
+    },
+    updateResults: function() {
+        var results = this.loadResults();
+
+        for(var i = 0; i < results.answers.length; i++) {
+            this.chart.series[i].data[0].update(results.answers[i].data, false);
+        }
+        this.chart.redraw();
+    },
+    graph: function() {
+        var self = this;
+        var graph = $('.highcharts');
+
+        var results = this.loadResults();
+
+        this.chart = new Highcharts.Chart({
+            chart: {
+                renderTo: graph[0],
+                type: 'column'
+            },
+            title: {
+                text: results.question
+            },
+            yAxis: {
+                min: 0,
+                max: 100,
+                title: {
+                    text: 'Percentage'
+                }
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: results.answers
+        });
+
+        setInterval(function() {
+            self.updateResults();
+        }, 1000);
     }
 };
 
